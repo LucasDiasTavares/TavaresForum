745f5eb4682705e87979f823fdbb9800a85e3256
Tavares <lucas.tavares1996@hotmail.com>
2020-09-21 11:04:15 -0300

    API Service with Fetch and CSRF TOKEN

---
+++ frontend/src/common/api.service.js
@@ -0,0 +1,23 @@
+import { CSRF_TOKEN } from "./csrf_token.js";
+
+async function getJSON(response) {
+  if (response.status === 204) return "";
+  return response.json();
+}
+
+// If wanna use imgs in your project change application/json to allow images
+function apiService(endpoint, method, data) {
+  const config = {
+    method: method || GET,
+    body: data !== undefined ? JSON.stringify(data) : null,
+    headers: {
+      "content-type": "application/json",
+      "X-CSRFTOKEN": CSRF_TOKEN,
+    },
+  };
+  return fetch(endpoins, config)
+    .then(getJSON)
+    .catch((error) => console.log(error));
+}
+
+export { apiService };
